b5ee746cc07be8751f4a9cabcbf5b55fa7a801f2
Pablo_CL <pablcl24@gmail.com>
2026-07-28 22:57:55 -0600

    Update clientes.php

--- admin/clientes.php
+++ admin/clientes.php
@@ -177,7 +177,7 @@ if (isset($pdo)) {
                                                             </div>
                                                             <div class="mb-3">
                                                                 <label class="form-label">Correo Electrónico</label>
-                                                                <input type="email" name="correo_usuario" class="form-control" value="<?= htmlspecialchars($c['correo_usuario']) ?>" required>
+                                                                <input type="email" name="email" value="<?php echo htmlspecialchars($cliente['email'] ?? ''); ?>" required>
                                                             </div>
                                                             <div class="mb-3">
                                                                 <label class="form-label">Teléfono</label>
